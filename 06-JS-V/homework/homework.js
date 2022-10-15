e91fbc1843e7f3439bcff0fcfd6a3d023bd3458c
angeldvilla <escarlata.2003@gmail.com>
2022-10-15 00:44:04 -0500

    Homework 5 y 6 finish

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,6 +8,7 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
+
   function Usuario(opciones) {
     this.usuario = opciones.usuario;
     this.nombre = opciones.nombre;
@@ -15,9 +16,23 @@ function crearUsuario() {
     this.password = opciones.password;
   }
   Usuario.prototype.saludar = function () {
-    return "Hola, mi nombre es " + this.nombre;
+    return `Hola, mi nombre es ${this.nombre}`;
   };
   return Usuario;
+
+  /*   class Usuario {
+    constructor(opciones) {
+      this.usuario = opciones.usuario;
+      this.nombre = opciones.nombre;
+      this.email = opciones.email;
+      this.password = opciones.password;
+    }
+    saludar() {
+      return `Hola, mi nombre es ${this.nombre}`;
+    }
+  }
+
+  return Usuario; */
 }
 
 function agregarMetodoPrototype(Constructor) {
